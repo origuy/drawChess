c0052b862282b26880f20d61ed4045987baef02c
Ori Guy <origuy2021@gmail.com>
2022-05-20 16:44:53 +0300

    .

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-
-let mainContainer = document.createElement("div")
-mainContainer.className = "mainContainer"
-document.body.appendChild(mainContainer)
-console.time("two")
+let mainContainer = document.createElement("div");
+mainContainer.className = "mainContainer";
+document.body.appendChild(mainContainer);
+console.time("two");
 for (let i = 0; i < 8; i++) {
 	let boxContainer = document.createElement("div");
 	boxContainer.className = "boxContainer";
 	mainContainer.appendChild(boxContainer);
-    	for (let z = 0; z < 8; z++) {
+	for (let z = 0; z < 8; z++) {
 		let box = document.createElement("div");
-		if (i % 2 === 0) {
-			z % 2 === 0 ? box.className = "black" : box.className = "white";
-		} else{
-			z % 2 === 0 ? box.className = "white" : box.className = "black";
-		}
+		i % 2 === 0
+			? z % 2 === 0
+				? (box.className = "black")
+				: (box.className = "white")
+			: z % 2 === 0
+			? (box.className = "white")
+			: (box.className = "black");
 		boxContainer.appendChild(box);
 	}
 }
-console.timeEnd("two")
-
-
+console.timeEnd("two");
